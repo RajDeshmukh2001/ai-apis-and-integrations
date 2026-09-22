12b68f884f9d96ab83779d6dcaa2ddc6ae9f20d6
Prithviraj Deshmukh <97602045+RajDeshmukh2001@users.noreply.github.com>
2026-09-22 20:43:45 +0530

    java || lesson 02 streaming response project setup

---
+++ java/lesson-02-streaming-response/src/main/java/com/ailearning/streamingresponse/Lesson02StreamingResponseApplication.java
@@ -0,0 +1,13 @@
+package com.ailearning.streamingresponse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson02StreamingResponseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lesson02StreamingResponseApplication.class, args);
+    }
+
+}
---
+++ java/lesson-02-streaming-response/src/test/java/com/ailearning/streamingresponse/Lesson02StreamingResponseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ailearning.streamingresponse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lesson02StreamingResponseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
