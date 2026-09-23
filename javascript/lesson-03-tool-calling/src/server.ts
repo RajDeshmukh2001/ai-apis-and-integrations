ab280d0075ff650ab2ea33099e844b1e19f04f0b
Prithviraj Deshmukh <97602045+RajDeshmukh2001@users.noreply.github.com>
2026-09-23 15:11:26 +0530

    javascript || lesson 03 Tool Calling express project setup

---
+++ javascript/lesson-03-tool-calling/src/server.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import "dotenv/config";
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.send("Tool Calling");
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
