dd931bc2bb635be435ce2006f274d4b9564aa771
Prithviraj Deshmukh <97602045+RajDeshmukh2001@users.noreply.github.com>
2026-09-25 14:30:22 +0530

    java || lesson 03 tool calling project setup

---
+++ java/lesson-03-tool-calling/src/main/java/com/ailearning/toolcalling/Lesson03ToolCallingApplication.java
@@ -0,0 +1,13 @@
+package com.ailearning.toolcalling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson03ToolCallingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lesson03ToolCallingApplication.class, args);
+    }
+
+}
---
+++ java/lesson-03-tool-calling/src/test/java/com/ailearning/toolcalling/Lesson03ToolCallingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ailearning.toolcalling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lesson03ToolCallingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
